01327992a52146f5acbd66f7420b4a46b5177e94
Sarah P <lovesarahinc@gmail.com>
2025-06-30 20:43:09 -0400

    Not specifying a localhost in the header.

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ import cors from 'cors';
 const app = express();
 
 
-app.use(cors({
-    origin: 'http://localhost:3001',
-}))
+app.use(cors())
 app.use(express.json());
 
 app.use('/api/companies', companyRoutes);
